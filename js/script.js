4a23a6abcf9380e4fdb6b27fc31db2e12137e22c
Shimanto Sarker <104753179+Shimanto23@users.noreply.github.com>
2025-03-15 04:31:01 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -51,49 +51,7 @@ $(document).ready(function() {
 	    }
 	});
 
-
 	
-
-
-	var skillsTopOffset = $(".skillsSection").offset().top;
-	var statsTopOffset = $(".statsSection").offset().top;
-	var countUpFinished = false;
-	$(window).scroll(function() {
-
-		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-
-			$('.chart').easyPieChart({
-		        easing: 'easeInOut',
-		        barColor: '#fff',
-		        trackColor: false,
-		        scaleColor: false,
-		        lineWidth: 4,
-		        size: 152,
-		        onStep: function(from, to, percent) {
-		        	$(this.el).find('.percent').text(Math.round(percent));
-		        }
-		    });
-
-
-		}
-
-
-		if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
-			$(".counter").each(function() {
-				var element = $(this);
-				var endVal = parseInt(element.text());
-
-				element.countup(endVal);
-			})
-
-			countUpFinished = true;
-
-		}
-
-
-	});
-
-
 	$("[data-fancybox]").fancybox(); 
 
 	$("#filters a").click(function() {
